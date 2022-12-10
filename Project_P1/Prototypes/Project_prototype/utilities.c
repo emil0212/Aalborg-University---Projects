f8fb8ee8ceec56f9530fc69419fec2a8cba1bc19
Tobias Tj <tobiastjornelund@gmail.com>
2022-12-10 02:05:21 +0100

    d

--- Project_P1/Prototypes/Project_prototype/utilities.c
+++ Project_P1/Prototypes/Project_prototype/utilities.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "utilities.h"
 
+//This function will Exit the program if there is no file or no file found//
+
 void validate_file_pointer(FILE * file)
 {
     if (file == NULL)
@@ -9,6 +11,16 @@ void validate_file_pointer(FILE * file)
         exit(EXIT_FAILURE);
     }
 }
+
+/**
+ *
+ * @param lat1
+ * @param lon1
+ * @param lat2
+ * @param lon2
+ * @return
+ */
+//This function is to measure the distance between the input (given in coordinates) and the supermarkets//
 
 double distance(double lat1, double lon1, double lat2, double lon2)
 {
@@ -28,17 +40,15 @@ double distance(double lat1, double lon1, double lat2, double lon2)
     return dist;
 }
 
-/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-/*::  This function converts decimal degrees to radians             :*/
-/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+//This function converts decimal degrees to radians//
+
 double deg2rad(double deg)
 {
     return (deg * pi / 180);
 }
 
-/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-/*::  This function converts radians to decimal degrees             :*/
-/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+//This function converts radians to decimal degrees//
+
 double rad2deg(double rad)
 {
     return (rad * 180 / pi);
