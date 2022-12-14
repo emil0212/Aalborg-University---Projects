27d284903ef4020649896c084e4750023996d833
BuildTools <unconfigured@null.spigotmc.org>
2022-12-14 10:19:24 +0100

    06-12-2022 16:30 create_store_prices() function -> update_store_database_with_prices()

---
+++ cmake-build-debug/Project_P1/Test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: K:/Aalborg-University---Projects/Project_P1/Test
+# Build directory: K:/Aalborg-University---Projects/cmake-build-debug/Project_P1/Test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
