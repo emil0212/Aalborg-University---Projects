2b8a7860866a14f0213f569b0ab8216bba97cd6f
BuildTools <unconfigured@null.spigotmc.org>
2022-12-14 10:03:48 +0100

    06-12-2022 16:30 create_store_prices() function -> update_store_database_with_prices()

--- Project_P1/Prototypes/Project_prototype/store_management.c
+++ Project_P1/Prototypes/Project_prototype/store_management.c
@@ -2,17 +2,30 @@
 #include "utilities.h"
 #include "store_management.h"
 
+
+/**
+ * This is a pointer of type store_db function.
+ * It's purpose is to initialize the file management system creating an dynamically allocated array of type store_db.
+ * This is array is then filled with information using several sister functions.
+ * @param session  | Of type userdata consisting of all information the user put as an input
+ * @return | The function is a pointer of type store_db which means it will return a pointer to the allocated array arrayOfStoreInfo
+ */
 store_db * create_store_database(userdata session)
 {
+    //Variable declarations
     char filename[20];
     FILE *db;
+
+    //Allocating space in the memory for MAX_STORES amount of type store_db
     store_db* arrayOfStoreInfo = malloc(MAX_STORES * sizeof *arrayOfStoreInfo);
 
+
+    //Loop through each store
     for (int k = 0; k < MAX_STORES; k++)
     {
-        sprintf(filename, "%d_info.txt", k);
-        db = fopen(filename, "r");
-        validate_file_pointer(db);
+        sprintf(filename, "%d_info.txt", k); //Change the file name to the desired store
+        db = fopen(filename, "r");          //Open the file
+        validate_file_pointer(db);                    //Validate file
         arrayOfStoreInfo[k] = collect_store_info(db);
         arrayOfStoreInfo[k].distance = distance(session.location_x, session.location_y,
                                                 arrayOfStoreInfo[k].x_coordinates, arrayOfStoreInfo[k].y_coordinates);
@@ -22,6 +35,11 @@ store_db * create_store_database(userdata session)
     return arrayOfStoreInfo;
 }
 
+/**
+ * This is a void function with no return value.
+ * It's purpose is to assign prices to all stores in the parameter using a sister function.
+ * @param arrayOfStorePrices | array of type store_db consisting of objects of type store_db
+ */
 void update_store_database_with_prices(store_db arrayOfStorePrices[])
 {
     char filename[20];
@@ -36,23 +54,42 @@ void update_store_database_with_prices(store_db arrayOfStorePrices[])
     }
 }
 
+/**
+ * This is a pointer of type char function.
+ * It's purpose is to find the specific line the product name is located and return it.
+ * @param id    | An integer used to indicate the specific line where the product name is located
+ * @return      | This function returns a pointer to an array of characters pointing at the specific product name
+ */
 char * find_product_name(int id)
 {
-    char *temp = malloc(MAX_PRODUCTS * sizeof(char));
+    //Allocating memory for an array of chars
+    char *temp = malloc(30 * sizeof(char));
 
+    //Open datafile with the product names
     FILE * file = fopen("productslist.txt", "r");
+
+    //Validate file
     validate_file_pointer(file);
 
+    //Run through the lines in the file up to the given id without scanning the product names
     for (int i = 0; i < id; i++) {
         fscanf(file, " %*s");
     }
 
+    //Here we continue from the loop and scan the line number given from the id
     fscanf(file," %s", temp);
     fclose(file);
 
     return temp;
 }
 
+/**
+ * This is a void function with no return value.
+ * It's purpose is to scan store grocery prices from a file and assign the specific grocery name through another function.
+ * @param db                | A file pointer pointing at a specific store file containing data about groceries
+ * @param arrayOfStoreInfo  | array of type store_db consisting of objects of type store_db
+ * @param number            | An integer used to indicate the specific store from 0 to MAX_STORES
+ */
 void collect_list_of_groceries(FILE *db, store_db arrayOfStoreInfo[], int number)
 {
     for (int i = 0; i < MAX_PRODUCTS; i++)
@@ -62,6 +99,12 @@ void collect_list_of_groceries(FILE *db, store_db arrayOfStoreInfo[], int number
     }
 }
 
+/**
+ * This is a function of type store_db
+ * It's purpose is to scan store information from a file
+ * @param db    | A file pointer pointing at a specific store file containing data about store info
+ * @return      | The function returns a struct of store_db containing information about a store
+ */
 store_db collect_store_info(FILE *db)
 {
     store_db storeInfo;
