c036abf31fca6b62769fabeadb6dbe347f2241cb
Emil Priv <global52600894@gmail.com>
2022-12-14 11:24:59 +0100

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: K:/Aalborg-University---Projects
-# Build directory: K:/Aalborg-University---Projects/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Project_P1/Prototypes")
-subdirs("Project_P1/Solution")
-subdirs("Project_P1/Test")
--- cmake-build-debug/Project_P1/Test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: K:/Aalborg-University---Projects/Project_P1/Test
-# Build directory: K:/Aalborg-University---Projects/cmake-build-debug/Project_P1/Test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
